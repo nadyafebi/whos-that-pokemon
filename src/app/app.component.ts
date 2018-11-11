beb5c8b1fe898e4ec6b1155b4d3438b18e590c42
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2018-11-10 16:28:24 -0800

    Get random Pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { PokemonService } from './service/pokemon.service';
+import { Observable } from 'rxjs';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'who-is-that-pokemon';
+export class AppComponent implements OnInit {
+  randomPokemon: Pokemon;
+
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit() {
+    this.getRandomPokemon();
+  }
+
+  getRandomPokemon() {
+    const randomNum = Math.floor(Math.random() * 807) + 1;
+    this.pokemonService.getPokemon(randomNum)
+      .subscribe((pokemon: Pokemon) => {
+        this.randomPokemon = pokemon;
+      });
+  }
 }
